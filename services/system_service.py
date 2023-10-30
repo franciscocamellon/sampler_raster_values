767ac883b03d0b74e782add8ab6f83a86901e4be
franciscocamellon <franciscocamellon@gmail.com>
2023-10-30 14:59:07 -0300

    Add docstrings

--- services/system_service.py
+++ services/system_service.py
@@ -41,9 +41,17 @@ from qgis.core import QgsField, QgsPointXY, QgsRasterLayer
 class SystemService:
 
     def __init__(self):
+        """
+        Constructor for the SystemService class.
+        """
         pass
 
     def getDateRange(self, fileName):
+        """
+        Extracts a date range from a filename and returns it as a tuple of start and end dates.
+        :param fileName: The name of the file containing the date range information.
+        :returns: A tuple (startDate, endDate) as datetime.date objects.
+        """
         parts = self.splitString(fileName, '.')
         dateRangeStr = parts[1]
         startDateStr, endDateStr = self.splitString(dateRangeStr, '_')
@@ -54,13 +62,30 @@ class SystemService:
 
     @staticmethod
     def isDateWithinRange(date, startDate, endDate):
-        # Check if a date is within a specified date range
+        """
+        Check if a date is within a specified date range.
+        :param date: The date to check.
+        :param startDate: The start date of the range.
+        :param endDate: The end date of the range.
+        :returns: True if the date is within the range, False otherwise.
+        """
         return startDate <= date <= endDate
 
     @staticmethod
     def formatDate(stringDate):
+        """
+        Converts a string date in the format 'YYYYMMDD' to a datetime.date object.
+        :param stringDate: The string date to format.
+        :returns: A datetime.date object.
+        """
         return datetime.strptime(stringDate, "%Y%m%d").date()
 
     @staticmethod
     def splitString(string, character):
+        """
+        Splits a string using a specified character and returns a list of substrings.
+        :param string: The string to split.
+        :param character: The character to use as a delimiter.
+        :returns: A list of substrings.
+        """
         return string.split(character)
